0ec5df7173f00b0cadcb4d7210208ef605278b15
Toni Sfetcu <toni.sfetcu@brainworks.ro>
2023-01-28 18:40:39 +0200

    build

--- sw.js
+++ sw.js
@@ -151,7 +151,7 @@ define(['./workbox-d2a14864'], (function (workbox) { 'use strict';
     "revision": "1b08fd675c565a74daa0053b8a744df3"
   }, {
     "url": "/",
-    "revision": "100eb9e1d21c1bfd5692f45b6a0d0a01"
+    "revision": "a4e2f92910e553760b154e3a6cf34a98"
   }, {
     "url": "js/a.js",
     "revision": "b8eb5afe6c79d1f23d45e862d925064b"
