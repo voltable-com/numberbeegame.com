a6f9fc0a3b680138f09b3dd71a6e28ae7bec3b65
Toni Sfetcu <toni.sfetcu@brainworks.ro>
2023-01-28 14:27:00 +0200

    build

--- sw.js
+++ sw.js
@@ -81,9 +81,6 @@ define(['./workbox-d2a14864'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/hoisted.d9b3e35d.js",
-    "revision": null
-  }, {
     "url": "assets/index.2584db01.css",
     "revision": null
   }, {
@@ -151,7 +148,13 @@ define(['./workbox-d2a14864'], (function (workbox) { 'use strict';
     "revision": "1b08fd675c565a74daa0053b8a744df3"
   }, {
     "url": "/",
-    "revision": "b6a2d5bf6a86af03b1a09746170fedc6"
+    "revision": "9a9a2714296bd2b5a4127fdd7b24ac10"
+  }, {
+    "url": "js/a.js",
+    "revision": "b8eb5afe6c79d1f23d45e862d925064b"
+  }, {
+    "url": "js/m.js",
+    "revision": "efc2e6a0bca9e982dc3fee54bccd4f1f"
   }, {
     "url": "registerSW.js",
     "revision": "6a0a0c8ba795b804f5fac2886a79f79c"
@@ -162,8 +165,14 @@ define(['./workbox-d2a14864'], (function (workbox) { 'use strict';
     "url": "favicon.svg",
     "revision": "582b185ad3098aaf9196648dcea8fd84"
   }, {
+    "url": "favicon-192.png",
+    "revision": "0ff8a157563dd1b40ed8949659d22336"
+  }, {
+    "url": "favicon-512.png",
+    "revision": "ddfa1af0e3619e013d35033a4c42104f"
+  }, {
     "url": "manifest.webmanifest",
-    "revision": "98a710458b9ba195b407d0315fd5cb04"
+    "revision": "1efc9c2294438bccf3822945f5f9b766"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/")));
